3d4dc8470773ba56913e754462b99812f895d4bc
shourya9 <shouryamaheshwari09@gmail.com>
2021-05-29 14:45:22 +0530

    File manager

---
+++ main.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+let fs = require("fs");
+let path = require("path");
+let helpObj=require("./commands/help");
+let treeObj=require("./commands/tree");
+let organizeObj = require("./commands/organize");
+let inputArr=process.argv.slice(2);
+console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organize "directoryPath"
+// node main.js help
+let command = inputArr[0];
+switch (command) {
+    case "tree":
+        treeObj.treeKey(inputArr[1]);
+        break;
+    case "organize":
+        organizeObj.organizeKey(inputArr[1]);
+        break;
+    case "help":
+        helpObj.helpKey();
+        break;
+    default:
+        console.log("Please 🙏 Input Right command");
+        break;
+}
+
